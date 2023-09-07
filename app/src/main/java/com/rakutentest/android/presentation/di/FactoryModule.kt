dd86f5ccb9f2affc7986158abc665f4c76d171fc
sidneymaleo <sidney.maleo@socloz.com>
2023-09-07 23:31:31 +0200

    we initialize our ProductViewModel

--- app/src/main/java/com/rakutentest/android/presentation/di/FactoryModule.kt
+++ app/src/main/java/com/rakutentest/android/presentation/di/FactoryModule.kt
@@ -1,10 +1,34 @@
 package com.rakutentest.android.presentation.di
 
+import com.rakutentest.android.domain.useCase.product.DeleteLocalProductsUseCase
+import com.rakutentest.android.domain.useCase.product.GetLocalProductsUseCase
+import com.rakutentest.android.domain.useCase.product.GetRemoteProductDetailsUseCase
+import com.rakutentest.android.domain.useCase.product.GetRemoteProductsUseCase
+import com.rakutentest.android.presentation.viewModel.Product.ProductViewModelFactory
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 class FactoryModule {
+
+    @Singleton
+    @Provides
+    fun provideProductViewModelFactory(
+        getRemoteProductsUseCase: GetRemoteProductsUseCase,
+        getRemoteProductDetailsUseCase: GetRemoteProductDetailsUseCase,
+        getLocalProductsUseCase: GetLocalProductsUseCase,
+        deleteLocalProductsUseCase: DeleteLocalProductsUseCase
+    ): ProductViewModelFactory {
+
+        return ProductViewModelFactory(
+            getRemoteProductsUseCase = getRemoteProductsUseCase,
+            getRemoteProductDetailsUseCase = getRemoteProductDetailsUseCase,
+            getLocalProductsUseCase = getLocalProductsUseCase,
+            deleteLocalProductsUseCase = deleteLocalProductsUseCase
+        )
+    }
 }
