a72759769826c5582cacca23b2c346da511f3ef9
sidneymaleo <sidney.maleo@socloz.com>
2023-09-09 14:20:35 +0200

    we build our fake BuyBox repository

--- app/src/test/java/com/rakutentest/android/domain/repository/FakeBuyBoxRepository.kt
+++ app/src/test/java/com/rakutentest/android/domain/repository/FakeBuyBoxRepository.kt
@@ -1,4 +1,26 @@
 package com.rakutentest.android.domain.repository
 
-class FakeBuyBoxRepository {
+import com.rakutentest.android.data.model.dataLocal.BuyboxRoom
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+
+class FakeBuyBoxRepository: BuyBoxRepository {
+
+    override fun getBuyBox(productId: Int): Flow<BuyboxRoom> {
+        return flow {
+            emit(
+                BuyboxRoom(
+                    id = 1,
+                    salePrice = 689.99f,
+                    advertType = "NEW",
+                    advertQuality = "NEW",
+                    saleCrossedPrice = 859f,
+                    salePercentDiscount = 19,
+                    isRefurbished = false,
+                    productId = 6035914280
+                )
+            )
+        }
+    }
+
 }
