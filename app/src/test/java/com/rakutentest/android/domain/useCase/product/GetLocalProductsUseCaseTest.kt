6bd94a4b1c99fd6a6a976fadb52e0ab238b0e54b
sidneymaleo <sidney.maleo@socloz.com>
2023-09-10 11:33:25 +0200

    we test who to retrieve local product list

--- app/src/test/java/com/rakutentest/android/domain/useCase/product/GetLocalProductsUseCaseTest.kt
+++ app/src/test/java/com/rakutentest/android/domain/useCase/product/GetLocalProductsUseCaseTest.kt
@@ -1,12 +1,36 @@
 package com.rakutentest.android.domain.useCase.product
 
+import app.cash.turbine.test
+import com.google.common.truth.Truth
+import com.rakutentest.android.domain.repository.FakeProductRepository
 import org.junit.Assert.*
-
 import org.junit.Before
+import org.junit.runner.RunWith
+import org.mockito.junit.MockitoJUnitRunner
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runTest
+import org.junit.Test
 
+
+@ExperimentalCoroutinesApi
+@RunWith(MockitoJUnitRunner::class)
 class GetLocalProductsUseCaseTest {
+
+    private lateinit var getLocalProductsUseCase: GetLocalProductsUseCase
+    private lateinit var fakeProductRepository: FakeProductRepository
 
     @Before
     fun setUp() {
+        fakeProductRepository = FakeProductRepository()
+        getLocalProductsUseCase = GetLocalProductsUseCase(productRepository = fakeProductRepository)
+    }
+
+    @Test
+    fun `retrieve all local products`() = runTest {
+         getLocalProductsUseCase.execute().test {
+             val localProducts = expectMostRecentItem()
+            Truth.assertThat(localProducts.size).isEqualTo(2)
+        }
+
     }
 }
